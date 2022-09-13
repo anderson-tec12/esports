c4edaa65ef5315411fd28227660fa66418f3eac5
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-09-13 07:43:36 -0300

    Initializing the API

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json([
+    { id: 1, name: "Anuncio 1" },
+    { id: 2, name: "Anuncio 2" },
+    { id: 3, name: "Anuncio 3" },
+    { id: 4, name: "Anuncio 4" },
+    { id: 5, name: "Anuncio 5" },
+  ]);
+});
+
+app.listen(3333, () => {
+  console.log("rodando na 3333");
+});
