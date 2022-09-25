296d69b153c28fdc2447462565c6f5c398c25d3f
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-09-25 15:17:55 -0300

    banco de dados condigurado

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,24 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-    { id: 4, name: "Anuncio 4" },
-    { id: 5, name: "Anuncio 5" },
-  ]);
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  const gameId = req.params.id;
+
+  return res.send(gameId);
+});
+
+app.get("/games/:id/discord", (req, res) => {
+  const gameId = req.params.id;
+
+  return res.json([]);
 });
 
 app.listen(3333, () => {
