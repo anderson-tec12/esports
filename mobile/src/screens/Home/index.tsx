6d3dbb306babfe2e6f9de5c43ebbc3eac7a89a23
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-09-26 10:03:34 -0300

    Conectando icom ap

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, FlatList } from "react-native";
 
 import Logo from "../../assets/logo.png";
 import { GameCard } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
-import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
+type GameProps = {
+  bannerUrl: string;
+  id: string;
+  title: string;
+  _count: { ads: number };
+};
+
 export function Home() {
+  const [listGames, setListGames] = useState<GameProps[]>([]);
+  useEffect(() => {
+    fetch("http://192.168.15.5:3333/games")
+      .then((res) => res.json())
+      .then((data) => setListGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.logo} />
@@ -19,7 +32,7 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={listGames}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
